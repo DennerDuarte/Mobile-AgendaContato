6f72792cddc75e7d982deb9fb92bf6d4faca476d
DennerDuarte <rm551938@fiap.com.br>
2024-09-03 22:55:28 -0300

    projeto finalizado

--- app/src/main/java/com/example/agendacontatoapi/AgendaContatosActivity.kt
+++ app/src/main/java/com/example/agendacontatoapi/AgendaContatosActivity.kt
@@ -4,14 +4,21 @@ import android.app.Activity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import okhttp3.Call
+import okhttp3.Callback
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response
+import java.io.IOException
 
 
 class AgendaContatosActivity : Activity(){
 
+    val URL_FIREBASE = "https://mobile---duarte-default-rtdb.firebaseio.com"
     override fun onCreate(bundle: Bundle?){
+
         super.onCreate(bundle)
         setContentView(R.layout.agenda_contato)
 
@@ -24,7 +31,9 @@ class AgendaContatosActivity : Activity(){
         val btnGravar = findViewById<Button>(R.id.btnGravar)
         val btnPesquisar = findViewById<Button>(R.id.btnPesquisar)
 
-        val contatoJson = """
+        btnGravar.setOnClickListener {
+
+            val contatoJson = """
             {
                 "nome":"${edtNome.text}",
                 "email":"${edtEmail.text}",
@@ -32,10 +41,32 @@ class AgendaContatosActivity : Activity(){
             }
         """.trimIndent()
 
-        val request = Request.Builder()
-            .url("")
-            .post(contatoJson.toRequestBody()).build()
+            val request = Request.Builder()
+                .url(URL_FIREBASE + "/contatos.json")
+                .post(contatoJson.toRequestBody()).build()
+
+            val response = object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    runOnUiThread {
+                        Toast.makeText(
+                            this@AgendaContatosActivity,
+                            "Erro ao gravar contato!",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    runOnUiThread {
+                        Toast.makeText(
+                            this@AgendaContatosActivity,
+                            "Contato gravado com sucesso",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+            }
+            client.newCall(request).enqueue(response)
+        }
     }
-
-
 }
